3f8fc4494215e712fdb37b8f4125344822e9b5c2
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2026-01-02 20:48:58 -0600

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ let a = true;
 let b = false;
 
 export default function Home() {
-  return <div>{(console.log(typeof a), console.log(b))}</div>;
+  return <div>{(console.log(a), console.log(b))}</div>;
 }
